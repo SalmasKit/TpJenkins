144268041f0e38e1995975302d5bbeacef7f70ee
Salma Barrak <salmabarrak26@gmail.com>
2026-03-30 11:16:36 +0100

    Initial Commit

---
+++ DeRien.java
@@ -0,0 +1,5 @@
+public class DeRien {
+    public static void main(String[] args) {
+        System.out.println("De Rien! The final job in the sequence has finished.");
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Jenkins has successfully compiled and run this class.");
+    }
+}
